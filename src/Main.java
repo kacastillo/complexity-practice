e07d173801ede7e16df683eea3d84c618f698b13
kelley <kelleycastillo@outlook.com>
2026-04-14 15:27:08 -0700

    finished w word problems w data structure

--- src/Main.java
+++ src/Main.java
@@ -205,14 +205,16 @@ public class Main {
   // The prices will be updated frequently throughout the day, and you need to efficiently update
   // and access the current price for each stock. The order of the ticker symbols is not important.
   // What would be a good choice of data structure?
-  // YOUR ANSWER HERE
+  
+  //HashMap<String, Double> tickerToPrice = new HashMap<>();
 
   // Suppose you are building a music player application where users can create playlists.
   // Songs can be added to the end of the playlist in the order the user chooses, and the user can
   // skip to the next or previous song. Most operations involve adding songs and accessing them by
   // their position in the playlist.
   // What would be a good choice of data structure?
-  // YOUR ANSWER HERE
+  
+  // ArrayList<String> playlist = new ArrayList<>();
 
   // Suppose you are developing a search feature that keeps track of the user's
   // recent search queries. You want to store the queries in the order they were made,
@@ -220,5 +222,8 @@ public class Main {
   // relatively small, and it is more important to preserve the order of the searches than
   // to optimize for fast lookups or deletions.
   // What would be a good choice of data structure?
-  // YOUR ANSWER HERE
-}
+
+  // ArrayList<String> recentSearches = new ArrayList<>();
+  // or linked list 
+  // LinkedList<String> recentSearches = new LinkedList<>();
+} 
